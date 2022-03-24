f1b6f2141a5db51a6ca936b05fdf40c79a1b6221
Kuni <kuni.ujiie@grapecity.com>
2022-03-24 10:13:27 +0900

    プロジェクト ファイルを追加します。

---
+++ WinUI3App1/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI3App1
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            // コードビハインドでItemSourceを設定する
+            //WinUI3DataGrid.ItemsSource = Customer.Customers();
+        }
+    }
+}
